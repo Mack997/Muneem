e001419b466535b734d51d908886aaf1788f7bf8
mack997 <mayankagarwal997@gmail.com>
2020-01-13 17:01:07 +0530

    Populating the Transaction to the View

--- app/src/main/java/com/scudderapps/muneem/Adapter/TransactionAdapter.java
+++ app/src/main/java/com/scudderapps/muneem/Adapter/TransactionAdapter.java
@@ -1,18 +1,22 @@
 package com.scudderapps.muneem.Adapter;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.scudderapps.muneem.Model.TransactionData;
 import com.scudderapps.muneem.R;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder> {
 
+
+    List<TransactionData> transactionData;
 
     @NonNull
     @Override
@@ -25,22 +29,34 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        TransactionData transactions = transactionData.get(position);
+        holder.catName.setText(transactions.getCategory());
+        holder.amount.setText(transactions.getAmount());
+        holder.comment.setText(transactions.getComment());
+        holder.date.setText(transactions.getDate());
+
     }
 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return transactionData.size();
+    }
+
+    public void setTransactionList(List<TransactionData> transactionData){
+        this.transactionData = transactionData;
+        notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView t1, t2, t3;
+        TextView catName, comment, amount, date;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            t1 = itemView.findViewById(R.id.cat_name);
-            t2 = itemView.findViewById(R.id.notes);
-            t3 = itemView.findViewById(R.id.amount);
+            catName = itemView.findViewById(R.id.cat_name);
+            comment = itemView.findViewById(R.id.notes);
+            amount = itemView.findViewById(R.id.amount);
+            date = itemView.findViewById(R.id.cat_date);
         }
     }
 }
--- app/src/main/java/com/scudderapps/muneem/Fragment/TransactionsFragment.java
+++ app/src/main/java/com/scudderapps/muneem/Fragment/TransactionsFragment.java
@@ -2,7 +2,10 @@ package com.scudderapps.muneem.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,13 +13,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.scudderapps.muneem.Adapter.CategoryAdapter;
 import com.scudderapps.muneem.Adapter.TransactionAdapter;
 import com.scudderapps.muneem.Dialogs.AddTransactionDialog;
+import com.scudderapps.muneem.Model.CategoryData;
+import com.scudderapps.muneem.Model.TransactionData;
 import com.scudderapps.muneem.R;
+import com.scudderapps.muneem.ViewModels.CategoryViewModel;
+import com.scudderapps.muneem.ViewModels.TransactionViewModel;
+
+import java.util.List;
 
 public class TransactionsFragment extends Fragment {
 
-    TransactionAdapter td;
+    TransactionAdapter transactionAdapter;
+    RecyclerView transactionView;
+    private TransactionViewModel transactionViewModel;
 
     public TransactionsFragment() {
     }
@@ -25,8 +37,19 @@ public class TransactionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_transactions, container, false);
-        RecyclerView transactionsView = rootView.findViewById(R.id.transaction_recycler_view);
+        transactionView = rootView.findViewById(R.id.transaction_recycler_view);
 
+        transactionViewModel = ViewModelProviders.of(this).get(TransactionViewModel.class);
+        transactionViewModel.getAllTransactions().observe(this, new Observer<List<TransactionData>>() {
+            @Override
+            public void onChanged(@Nullable List<TransactionData> transactionData) {
+                //setting up the data in recycler view.
+                transactionView.setLayoutManager(new LinearLayoutManager(getContext()));
+                transactionAdapter= new TransactionAdapter();
+                transactionView.setAdapter(transactionAdapter);
+                transactionAdapter.setTransactionList(transactionData);
+            }
+        });
         return rootView;
 
     }
